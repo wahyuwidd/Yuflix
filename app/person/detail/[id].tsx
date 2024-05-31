e13f42c5c9fc7f0bbb4d7e3af793ba536d868d64
wwxxc <woianjur@gmail.com>
2024-05-31 11:15:43 +0700

    first commit

--- app/person/detail/[id].tsx
+++ app/person/detail/[id].tsx
@@ -1,6 +1,6 @@
 import { useLocalSearchParams } from 'expo-router';
 import { useEffect, useState } from 'react';
-import { View, Text } from 'react-native'
+import { View, Text, ScrollView, Image } from 'react-native'
 
 const DetailPerson = () => {
     const { id } = useLocalSearchParams<{id: string}>();
@@ -14,8 +14,47 @@ const DetailPerson = () => {
         })
     })
     return (
-        <View className='bg-[#151515] h-full'>
-            {person && <Text className='text-white text-3xl font-bold p-4'>{person.name}</Text>}
+        <View className='bg-[#151515] h-full w-full'>
+            <ScrollView contentContainerStyle={{ alignItems: 'center', paddingVertical: 20 }}>
+                <View className='mt-20'>
+                    <View className='justify-center items-center'>
+                        <Image
+                            className='h-[250px] w-[250px] rounded-full'
+                            source={{ uri: 'https://image.tmdb.org/t/p/w500' + person?.profile_path }} />
+                    </View>
+                    <Text className='text-white mt-5 text-2xl text-center font-bold'>{person?.name}</Text>
+                    <Text className='text-gray-300 mt-2 text-sm text-center '>{person?.place_of_birth}</Text>
+                    <View className='bg-gray-600 p-3 rounded-full mt-5 items-center justify-center mx-4' style={{ flexDirection: 'row', flexWrap: 'wrap' }}>
+                        <View style={{ alignItems: 'center', marginRight: 10 }}>
+                            <Text className='text-white text-sm'>Gender</Text>
+                            <Text className='text-white text-sm'>{person?.gender}</Text>
+                        </View>
+                        <View style={{ height: '100%', width: 1, backgroundColor: 'white', marginHorizontal: 10 }} />
+                        <View style={{ alignItems: 'center', marginRight: 10 }}>
+                            <Text className='text-white text-sm'>Birthday</Text>
+                            <Text className='text-white text-sm'>{person?.birthday}</Text>
+                        </View>
+                        <View style={{ height: '100%', width: 1, backgroundColor: 'white', marginHorizontal: 10 }} />
+                        <View style={{ alignItems: 'center', marginRight: 10 }}>
+                            <Text className='text-white text-sm'>Known for</Text>
+                            <Text className='text-white text-sm'>{person?.known_for_department}</Text>
+                        </View>
+                        <View style={{ height: '100%', width: 1, backgroundColor: 'white', marginHorizontal: 10 }} />
+                        <View style={{ alignItems: 'center' }}>
+                            <Text className='text-white text-sm'>Popularity</Text>
+                            <Text className='text-white text-sm'>{person?.popularity}</Text>
+                        </View>
+                    </View>
+                    <View className='mx-4'>
+                        <Text className='text-white mt-5 text-xl font-bold'>Biography</Text>
+                        <Text className='text-gray-300 mt-2 text-sm'>{person?.biography}</Text>
+                    </View>
+                    <View className='mx-4'>
+                        <Text className='text-white mt-5 text-xl font-bold'>Movies</Text>
+                        <Text className='text-gray-300 mt-2 text-sm'>{person?.biography}</Text>
+                    </View>
+                </View>
+            </ScrollView>
         </View>
     )
 }
